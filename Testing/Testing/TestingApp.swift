7914ea74b4e0880e699b1565bc57a490ddb30654
BastianG09 <105691598+BastianG09@users.noreply.github.com>
2022-07-11 15:55:31 -0500

    Add files via upload

---
+++ Testing/Testing/TestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestingApp.swift
+//  Testing
+//
+//  Created by BastianC on 7/1/22.
+//
+
+import SwiftUI
+
+@main
+struct TestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
